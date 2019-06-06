882bd55ebd8f0c479c270da894c3833b2c1a864e
Julian Annen <julian.annen@web.de>
2019-06-06 12:27:56 +0200

    Merge branch 'master' of https://code.fbi.h-da.de/istjuanne/rfid
    # Conflicts:
    #	src/MoneyPiece.java
    #	src/Purse.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,15 +1,15 @@
 import java.util.Date;
 
 public class Transaction {
-    double m_oldValue;
-    double m_newValue;
-    double m_diffrence;
-    Date m_date;
+    double oldValue;
+    double newValue;
+    double diffrence;
+    Date date;
 
     public Transaction(double oldValue, double newValue) {
-        this.m_oldValue = oldValue;
-        this.m_newValue = newValue;
-        m_diffrence = m_oldValue - m_newValue;
-        m_date = new Date();
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+        diffrence = this.oldValue - this.newValue;
+        date = new Date();
     }
 }
